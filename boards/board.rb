2cb8b66bd36f4e72c9b7d67059275468010eb12b
Andrew <andy@andy.com>
2015-10-15 20:17:13 +1300

    first test passing

--- boards/board.rb
+++ boards/board.rb
@@ -5,7 +5,7 @@ class Board
   #   "xoxoxooo."
   #   "x..o...x."
   def load(input)
-    raise NotImplementedError
+	@state = input.chars
   end
 
   # Records a player's turn.
@@ -18,7 +18,11 @@ class Board
   #     7 | 8 | 9
   # Needs to raise InvalidPlacementError if the position is already filled
   def mark_turn(player, position)
-    raise InvalidPlacementError
+    if (state[position - 1] != '.')
+		raise InvalidPlacementError
+	else
+		@state[position - 1] = player
+	end
   end
 
   # True if all the squares have been filled
@@ -40,7 +44,7 @@ class Board
   end
 
 private
-
+	attr_accessor :state
   # This is optional and isn't tested
   # This needs to be a 2D array
   # e.g. [["x", "o", "x"], ["o", "x", "o"], ["o", "o", " "]]
